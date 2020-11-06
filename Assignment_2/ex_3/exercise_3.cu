04c277a8b39cbc0386cc89998031cd32052d3be0
Jonas Valfridsson <jonas@valfridsson.net>
2020-11-06 16:43:57 +0100

    latest

--- Assignment_2/ex_3/exercise_3.cu
+++ Assignment_2/ex_3/exercise_3.cu
@@ -1,6 +1,7 @@
-const float dx = 0.1;
-const float dy = 0.1;
-const float dz = 0.1;
+#include "stdio.h"
+#include <iostream>
+#include <chrono>
+
 
 struct Particle {
   float3 position;
@@ -18,7 +19,7 @@ __host__ __device__ float3 velocity_update(float3 velocity, float time) {
 }
 
 __global__ void gpu_step(int n, Particle *particles, float time) {
-  int i = threadIdx.x * blockIdx.x * blockDim.x;
+  int i = threadIdx.x + blockIdx.x * blockDim.x;
 
   if (i < n) {
     particles[i].velocity = velocity_update(particles[i].velocity, time);
@@ -29,26 +30,151 @@ __global__ void gpu_step(int n, Particle *particles, float time) {
   }
 }
 
-void cpu_step(int n, float3 *dv, Particle *particles, float time) {
+void cpu_step(int n, Particle *particles, float time) {
   for (int i = 0; i < n; i++) {
     particles[i].velocity = velocity_update(particles[i].velocity, time);
 
-    particles[i].position.x += dv[i].x;
-    particles[i].position.y += dv[i].y;
-    particles[i].position.z += dv[i].z;
+    particles[i].position.x += particles[i].velocity.x;
+    particles[i].position.y += particles[i].velocity.y;
+    particles[i].position.z += particles[i].velocity.z;
   }
 }
 
+float rand_float() {
+  return (float)(rand()) / ((float)(RAND_MAX));
+}
+
+void init_particles(int n, Particle *particles) {
+  for (int i = 0; i < n; i++) {
+    float3 pos, vel;
+
+    pos.x = rand_float();
+    pos.y = rand_float();
+    pos.z = rand_float();
+
+    vel.x = rand_float();
+    vel.y = rand_float();
+    vel.z = rand_float();
+
+    particles[i].position = pos;
+    particles[i].velocity = vel;
+  }
+}
+
+double mse_difference(int n, Particle *xp, Particle *yp) {
+  double mse = 0;
+  for (int i = 0; i < n; i++) {
+    mse += (xp[i].position.x - yp[i].position.x) * (xp[i].position.x - yp[i].position.x);
+    mse += (xp[i].position.y - yp[i].position.y) * (xp[i].position.y - yp[i].position.y);
+    mse += (xp[i].position.z - yp[i].position.z) * (xp[i].position.z - yp[i].position.z);
+
+    mse += (xp[i].velocity.x - yp[i].velocity.x) * (xp[i].velocity.x - yp[i].velocity.x);
+    mse += (xp[i].velocity.y - yp[i].velocity.y) * (xp[i].velocity.y - yp[i].velocity.y);
+    mse += (xp[i].velocity.z - yp[i].velocity.z) * (xp[i].velocity.z - yp[i].velocity.z);
+
+  }
+
+  return mse / (double)n;
+}
+
+void print_particles(int n, Particle *par) {
+  for (int i = 0; i < n; i++) {
+    std::cout << "n: " << i << " px: " << par[i].position.x << " py: " << par[i].position.y
+      << " pz: " << par[i].position.z << " vx: " << par[i].velocity.x << " vy: " << par[i].velocity.y
+      << " vz: " << par[i].velocity.z << std::endl;
+  }
+}
 
 int main(int argc, char **argv) {
-  int n_par = std::atoi(argv[1]), n_it =std::atoi(argv[2]);
+  int n_par = std::atoi(argv[1]), n_it =std::atoi(argv[2]), block_size = std::atoi(argv[3]);
 
-  float3 *dv;
-  Particle *par;
+  cudaError_t err;
 
-  // Allocate shared memory
-  cudaMallocManaged(&dv, sizeof(float3) * n_par);
-  cudaMallocManaged(&par, sizeof(Particle) * n_par);
+  Particle *par_host, *par_device, *par_device_result_on_host;
+
+
+  par_host = (Particle*)malloc(sizeof(Particle) * n_par);
+  init_particles(n_par, par_host);
+
+  //std::cout << "Particles on host:\n";
+  //print_particles(n_par, par_host);
+  //std::cout << "\n";
+
+  // Copy initial state to GPU
+  cudaMalloc(&par_device, sizeof(Particle)*n_par);
+  cudaMemcpy(par_device, par_host, sizeof(Particle) * n_par, cudaMemcpyHostToDevice);
+
+  // Synchronize any initial copy
+  err = cudaDeviceSynchronize();
+  if (err != cudaSuccess) {
+    printf("Error %s", cudaGetErrorString(err));
+  }
+
+  float t = 0;
+
+  // Simulate on CPU
+  auto start = std::chrono::steady_clock::now();
+  for (int i = 0; i < n_it; i++) {
+    cpu_step(n_par, par_host, t);
+
+    t += 1.0;
+  }
+
+  auto end = std::chrono::steady_clock::now();
+
+  std::cout
+    << "Simulating on CPU took: "
+    << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << "µs ≈ "
+    << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms.\n";
+
+
+  // reset time
+  t = 0;
+
+  // Simulate on GPU
+  start = std::chrono::steady_clock::now();
+  for (int i = 0; i < n_it; i++) {
+    gpu_step<<<(n_par / block_size) + 1, block_size>>>(n_par, par_device, t);
+
+    //err = cudaDeviceSynchronize();
+    //if (err != cudaSuccess) {
+    //printf("Error %s", cudaGetErrorString(err));
+    //}
+
+    t += 1.0;
+  }
+
+  err = cudaDeviceSynchronize();
+  if (err != cudaSuccess) {
+    printf("Error %s", cudaGetErrorString(err));
+  }
+
+  end = std::chrono::steady_clock::now();
+
+  std::cout
+    << "Simulating on GPU took: "
+    << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << "µs ≈ "
+    << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms.\n";
+
+
+  par_device_result_on_host = (Particle*)malloc(sizeof(Particle) * n_par);
+  cudaMemcpy(par_device_result_on_host, par_device, sizeof(Particle) * n_par, cudaMemcpyDeviceToHost);
+
+  //std::cout << "Particles from GPU:\n";
+  //print_particles(n_par, par_device_result_on_host);
+  //std::cout << "\n";
+
+  //std::cout << "Particles from CPU:\n";
+  //print_particles(n_par, par_host);
+  //std::cout << "\n";
+
+  double mse = mse_difference(n_par, par_device_result_on_host, par_host);
+  std::cout << "GPU - CPU mean squared error: " << mse << std::endl;
+
+
+  // Free memory
+  free(par_host);
+  cudaFree(par_device);
 
   return 0;
 }
